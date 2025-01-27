79412c3bf0269988630594a719f432128f3edce5
ice breaker <1324318532@qq.com>
2025-01-28 02:58:29 +0800

    chore(deps): upgrade

--- apps/host/src/index.ts
+++ apps/host/src/index.ts
@@ -1,9 +1,26 @@
+import type { JwtVariables } from 'hono/jwt'
 import { Hono } from 'hono'
 import { showRoutes } from 'hono/dev'
+import { HTTPException } from 'hono/http-exception'
+import { jwt, sign } from 'hono/jwt'
 import { getFirstIpRecord } from './get-dns-records'
-import { GET_TABLE, UPSERT } from './sql'
+import { scheduledTask } from './schedule'
+import { UPSERT } from './sql'
 
-const app = new Hono<{ Bindings: CloudflareBindings }>()
+type Variables = JwtVariables
+const app = new Hono<{ Bindings: CloudflareBindings, Variables: Variables }>()
+
+app.use('/*', async (c, next) => {
+  const jwtMiddleware = jwt({
+    secret: c.env.JWT_SECRET,
+  })
+  if (c.req.path.startsWith('/auth')) {
+    return await next()
+  }
+  else {
+    return jwtMiddleware(c, next)
+  }
+})
 
 app.get('/add', async (ctx) => {
   const { env } = ctx
@@ -20,26 +37,48 @@ app.get('/add', async (ctx) => {
   return ctx.text(`Added ${name}, Ip: ${firstIpRecord?.data}`)
 })
 
+app.get('/scheduled', async (ctx) => {
+  const tasks = await scheduledTask(ctx.env)
+  return ctx.text(`${tasks.length}`)
+})
+
+app.post('/auth/login', async (c) => {
+  const { email, password } = await c.req.json()
+  const res = await c.env.DB.prepare('').bind([email, password]).run()
+  const user = res.results[0]
+  if (user) {
+    const payload = {
+      email,
+      id: user.id,
+    }
+    const token = await sign(payload, c.env.JWT_SECRET)
+    c.header('Set-Cookie', `token=${token}; Path=/; HttpOnly; Secure; SameSite=Strict`)
+    return c.text('Login successful')
+  }
+  else {
+    throw new HTTPException(400, {
+      message: 'User not found',
+    })
+  }
+
+  // res.results[0]
+})
+
+app.get('/self', async (c) => {
+  const payload = c.get('jwtPayload')
+  return c.json(payload)
+})
+
 showRoutes(app, {
   verbose: true,
 })
 
 export default {
   fetch: app.fetch,
-  async scheduled(_event, env, ctx) {
-    const res = await env.DB.prepare(GET_TABLE).run()
-    const hostList = (res).results?.map((x) => {
-      return {
-        domain: x.Domain as string,
-        ip: x.Ip,
-      }
-    }) ?? []
-    const p = Promise.allSettled(hostList.map(async (x) => {
-      const firstIpRecord = await getFirstIpRecord(x.domain)
-      if (firstIpRecord) {
-        await env.DB.prepare(UPSERT).bind(x.domain, firstIpRecord.data).run()
-      }
-    }))
-    ctx.waitUntil(p)
+  // https://developers.cloudflare.com/workers/runtime-apis/handlers/scheduled/
+  // npx wrangler dev --test-scheduled
+  // curl "http://localhost:8787/__scheduled?cron=*+*+*+*+*"
+  scheduled(_event, env, ctx) {
+    ctx.waitUntil(scheduledTask(env))
   },
 } satisfies ExportedHandler<CloudflareBindings>
