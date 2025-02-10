b1256291e427a5b09c17669cc9833ff42b18a46d
coderdkai <coderdkai@gmail.com>
2025-02-10 18:50:27 +0800

    feat: add get sub domains methods

--- packages/dns/src/index.ts
+++ packages/dns/src/index.ts
@@ -1,9 +1,11 @@
 import type { DnsRecord } from './types'
 import { getDnsRecords, getFirstIpRecord } from './get-dns-records'
+import { getSubDomains } from './get-sub-domains'
 
 export {
   getDnsRecords,
   getFirstIpRecord,
+  getSubDomains,
 }
 
 export type {
