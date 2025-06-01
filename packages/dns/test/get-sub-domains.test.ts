c35ca3c747cf2ad1e915da960ef68f3a2bb9cf50
ice breaker <1324318532@qq.com>
2025-06-01 14:39:12 +0800

    chore(deps): upgrade

--- packages/dns/test/get-sub-domains.test.ts
+++ packages/dns/test/get-sub-domains.test.ts
@@ -1,9 +1,9 @@
-import { getSubDomains } from '@/get-sub-domains'
 import { isCI } from 'ci-info'
+import { getSubDomains } from '@/get-sub-domains'
 
 describe.skipIf(isCI)('get-sub-domains', () => {
   it('sonofmagic.top', async () => {
     const subDomains = await getSubDomains('sonofmagic.top')
-    expect(subDomains).toMatchSnapshot()
+    expect(subDomains.sort()).toMatchSnapshot()
   })
 })
